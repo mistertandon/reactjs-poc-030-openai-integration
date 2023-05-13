3a04106d26be56a4a8c7e5b966262aad31f206b5
mistertandon <enggparveshtandon@gmail.com>
2023-05-13 17:43:12 +0530

    feat: Implement prompt form using react-hook-form

--- src/components/ContentGenerator/ContentGenerator.jsx
+++ src/components/ContentGenerator/ContentGenerator.jsx
@@ -1,5 +1,76 @@
+import { useForm } from "react-hook-form";
+
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// const response = await openai.createCompletion({
+//   model: "text-davinci-003",
+//   prompt: "",
+//   temperature: 0.7,
+//   max_tokens: 256,
+//   top_p: 1,
+//   frequency_penalty: 0,
+//   presence_penalty: 0,
+// });
+
 const ContentGenerator = () => {
-  return <section>Hello from section</section>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const { ai_prompt } = data;
+  };
+
+  return (
+    <article>
+      <section>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form__field--container">
+            <label
+              htmlFor="formfield--ai_prompt"
+              className="form__field__input--label"
+            >
+              What you are thinking to solve, let me know
+            </label>
+            <input
+              id="formfield--ai_prompt"
+              className="form__field__input"
+              {...register("ai_prompt", {
+                validate: {
+                  shouldNotEmpty: (value) => {
+                    const emptyCheck =
+                      typeof value === "undefined" || value === ""
+                        ? false
+                        : true;
+
+                    return emptyCheck || "Please enter a query";
+                  },
+                },
+              })}
+            />
+            {errors?.ai_prompt && (
+              <p className="form__field__input--error">
+                {errors?.ai_prompt?.message}
+              </p>
+            )}
+          </div>
+
+          <div className="form__field--container">
+            <input type="submit" className="form__field__button--submit" />
+          </div>
+        </form>
+      </section>
+    </article>
+  );
 };
 
 export default ContentGenerator;
