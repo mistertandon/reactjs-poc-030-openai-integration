23620ceb1cb454bd95fb09437b800c38b783ddbf
mistertandon <enggparveshtandon@gmail.com>
2023-05-14 06:32:06 +0530

    feat: Modify Outline component to render outline using ContentView component

--- src/components/Outline/Outline.jsx
+++ src/components/Outline/Outline.jsx
@@ -1,6 +1,14 @@
+import { ContentView } from "./../";
 import "./Outline.scss";
-const Outline = ({ outline = ""}) => {
-  return <>OUTLNE HELLO</>;
+
+const Outline = ({ outline = "" }) => {
+  return (
+    <>
+      {outline.map((item, idx) => {
+        return <ContentView key={`view-${idx}`} content={item} />;
+      })}
+    </>
+  );
 };
 
 export default Outline;
