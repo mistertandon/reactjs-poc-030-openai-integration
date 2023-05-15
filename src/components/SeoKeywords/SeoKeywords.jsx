e033c5c5d62b9962532ca1fa71e55fbc8e202d92
mistertandon <enggparveshtandon@gmail.com>
2023-05-15 08:15:10 +0530

    feat: Use React Icons in app

--- src/components/SeoKeywords/SeoKeywords.jsx
+++ src/components/SeoKeywords/SeoKeywords.jsx
@@ -1,13 +1,23 @@
 import { useId } from "react";
 import "./SeoKeywords.scss";
 import { ContentView } from "./../";
+import { CheckmarkDoneCircleOutline } from "react-ionicons";
+
 const SeoKeywords = ({ keywords }) => {
   return (
     <>
       {keywords.map((keyword) => {
         return (
-          <div className="seo-keyword--container">
-            <ContentView key={useId()} content={keyword} />
+          <div key={useId()} className="seo-keyword--container">
+            <CheckmarkDoneCircleOutline
+              className="seo-keyword__icon"
+              color={"#00000"}
+              height="16px"
+              width="16px"
+            />
+            <div className="seo-keyword__label">
+              <ContentView content={keyword} />
+            </div>
           </div>
         );
       })}
